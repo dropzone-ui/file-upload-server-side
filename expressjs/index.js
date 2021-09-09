35cfbf330b8d4bb3c8e3647605af0d9923d45ac8
Jin Jose Manuel <43678736+JinSSJ3@users.noreply.github.com>
2021-09-09 11:39:33 -0500

    Update index.js

--- expressjs/index.js
+++ expressjs/index.js
@@ -27,7 +27,7 @@ app.listen(port, () =>
 
 
 
-app.post('/upload-avatar', async (req, res) => {
+app.post('/upload-my-file', async (req, res) => {
     try {
         if(!req.files) {
             res.send({
@@ -35,20 +35,20 @@ app.post('/upload-avatar', async (req, res) => {
                 message: 'No file uploaded'
             });
         } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let avatar = req.files.avatar;
+            //Use the name of the input field (i.e. "file") to retrieve the uploaded file
+            let file = req.files.file;
             
             //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('./uploads/' + avatar.name);
+            file.mv('./uploads/' + avatar.name);
 
             //send response
             res.send({
                 status: true,
                 message: 'File is uploaded',
                 data: {
-                    name: avatar.name,
-                    mimetype: avatar.mimetype,
-                    size: avatar.size
+                    name: file.name,
+                    mimetype: file.mimetype,
+                    size: file.size
                 }
             });
         }
@@ -57,39 +57,4 @@ app.post('/upload-avatar', async (req, res) => {
     }
 });
 
-app.post('/upload-photos', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            let data = []; 
-    
-            //loop all files
-            _.forEach(_.keysIn(req.files.photos), (key) => {
-                let photo = req.files.photos[key];
-                
-                //move photo to uploads directory
-                photo.mv('./uploads/' + photo.name);
-
-                //push file details
-                data.push({
-                    name: photo.name,
-                    mimetype: photo.mimetype,
-                    size: photo.size
-                });
-            });
-    
-            //return response
-            res.send({
-                status: true,
-                message: 'Files are uploaded',
-                data: data
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
 });
