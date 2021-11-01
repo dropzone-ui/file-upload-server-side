a484e5d74b50fe38be2951015f61a7c2f0ec55ec
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2021-10-31 21:46:32 -0500

    [EXAMPLE]

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,60 +1,55 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const _ = require('lodash');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const _ = require("lodash");
 
 const app = express();
 
 // enable files upload
-app.use(fileUpload({
-    createParentPath: true
-}));
-
+app.use(fileUpload({ createParentPath: true }));
 //add other middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-//start app 
+//start app
 const port = process.env.PORT || 2800;
 
-app.listen(port, () => 
-  console.log(`App is listening on port ${port}.`)
-);
-
-
-
-app.post('/upload-my-file', async (req, res) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            //Use the name of the input field (i.e. "file") to retrieve the uploaded file
-            let file = req.files.file;
-            
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            file.mv('./uploads/' + file.name);
-
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: file.name,
-                    mimetype: file.mimetype,
-                    size: file.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
+app.post("/upload-my-file", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded",
+        payload: {},
+      });
+    } else {
+      //Use the name of the input field (i.e. "file") to retrieve the uploaded file
+      let file = req.files.file;
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      file.mv("./uploads/" + file.name);
+      //send response
+      res.send({
+        status: true,
+        message: "File was uploaded successfully",
+        payload: {
+          name: file.name,
+          mimetype: file.mimetype,
+          size: file.size,
+          path: "/files/uploads/",
+          url: "https://my-ftp-server.com/bjYJGFYgjfVGHVb",
+        },
+      });
     }
-});
-
+  } catch (err) {
+    res.status(500).send({
+      status: false,
+      message: "Unspected problem",
+      payload: {},
+    });
+  }
 });
